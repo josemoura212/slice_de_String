c61460189fae11e7154fcc00891edb13487d7f87
José Augusto <josemoura212@gmail.com>
2024-01-03 19:40:53 -0300

    Fix string case in Cargo.toml and add function to extract first word in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let text = String::from("Olá mundo!");
+    let first = primeira_palavra(&text);
+
+    println!("Primeira palavra: {}", first);
+}
+
+fn primeira_palavra(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
